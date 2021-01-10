3cf9969ab2edd354728b0a9cd3cc4882de5fec24
Lorenzo C <lorenzo.cavada1@gmail.com>
2021-01-10 21:01:56 +0100

    Fixed alt warnings

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import { default as logo } from './logo.svg';
 function Navbar() {
   return (
     <nav className='py-10 flex h-16 items-center justify-center border-b-2 shadow-md fixed w-full z-50 bg-white bg-opacity-75 top-0'>
-      <img src={logo} className='mt-2' width='170' heigth='auto'/>
+      <img alt="logo" src={logo} className='mt-2' width='170' heigth='auto'/>
     </nav>      
   )
 }
--- src/Picture.js
+++ src/Picture.js
@@ -5,13 +5,13 @@ function Picture(props) {
     return (
         <div data-aos-delay={Math.floor(Math.random() * 10)*50} data-aos="zoom-in" data-aos-once="true">
             <div  className="transform text-center max-w-sm rounded overflow-hidden shadow-lg transition duration-400 hover:scale-105">
-                <img className='w-full'  src={props.url} /> 
+                <img alt={props.label} className='w-full'  src={props.url} /> 
                 <div className='h-14'>
                     <p className='text-xl mb-2 font-poppins'>{props.label}</p>
                 </div>
             </div>    
         </div>   
-        
+
     ) 
 }
  
