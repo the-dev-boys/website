baa6dd5597dd92a967cb2b060b6042865cce53f6
Lorenzo C <lorenzo.cavada1@gmail.com>
2021-01-12 19:17:17 +0100

    Added escape handle in modal

--- src/Picture.js
+++ src/Picture.js
@@ -1,22 +1,18 @@
-import React, { useState } from "react";
-import PictureModal from "./PictureModal";
+import React from "react";
 
 
-function Picture(props) {
-    const [show, setShow] = useState(false)
 
+function Picture(props) {    
     return (
         <div>
-            <div data-aos-delay={Math.floor(Math.random() * 10)*50} data-aos="zoom-in" data-aos-once="true" onClick={() => setShow(true)}>
+            <div data-aos-delay={Math.floor(Math.random() * 10)*50} data-aos="zoom-in" data-aos-once="true" onClick={() => props.setSelectedPicture({url: props.url, label: props.label})}>
                 <div  className="transform text-center max-w-sm rounded overflow-hidden shadow-lg transition duration-400 hover:scale-105">
                     <img alt={props.label} className='w-full'  src={props.url} /> 
                     <div className='h-14'>
                         <p className='text-xl mb-2 font-poppins'>{props.label}</p>
                     </div>
                 </div>    
-            </div>
-            
-            <PictureModal onClose={() => setShow(false)} show={show} url={props.url}/>            
+            </div>         
         </div>
 
 
--- src/PictureModal.js
+++ src/PictureModal.js
@@ -1,21 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-function PictureModal(props) {
+function PictureModal(props){
 
-    if (!props.show) {
-        return null
+    const onEscape = ({key}) => {
+        if (key === "Escape"){
+            props.onClose()
+        }
     }
 
-    return (
+    useEffect(() => {
+        document.addEventListener("keyup", onEscape)
+        return () => document.removeEventListener("keyup", onEscape)
+    }, )
 
+    return (        
         <div className="flex justify-center items-center fixed z-10 inset-0 overflow-y-auto">
             <div className="text-center p-10">
                 <div className="fixed inset-0 transition-opacity" onClick={props.onClose} aria-hidden="true">
                     <div className="absolute inset-0 bg-gray-500 opacity-75"></div>
                 </div>
-                <span className="hidden sm:inline-block sm:align-middle" aria-hidden="true">&#8203;</span>
-                <div className="flex rounded-xl shadow-xl transform transition-all max-w-4xl" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
-                    <img className="inline-block rounded-xl shadow-xl" alt="Piture" src={props.url}/>                    
+                <div className="flex overflow-hidden rounded-xl shadow-xl transform transition-all max-w-4xl" srole="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                    <img alt="Piture" src={props.url}/>                    
                 </div>
             </div>
         </div>
